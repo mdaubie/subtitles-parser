639795b0942cec4f1cb9ad9566a46a4df8b1f31b
DaubieMatthieu <matthieu.daubie@eleve.isep.fr>
2023-05-04 23:41:55 +0200

    refactor(test): encapsulate test objects in suppliers, so that tests use same data but different objects, and do not interfere between them

--- src/test/java/io/github/mdaubie/subtitlesparser/ParserTest.java
+++ src/test/java/io/github/mdaubie/subtitlesparser/ParserTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static io.github.mdaubie.subtitlesparser.constants.SUB_FILE_FORMATS.SUB_RIP_FORMAT;
@@ -15,9 +16,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ParserTest {
     @ParameterizedTest
     @MethodSource("parse")
-    void parse(Format<? extends SubtitlesFile> format, String text, SubtitlesFile expectedResult) throws Exception {
+    void parse(Format<? extends SubtitlesFile> format, String text, Supplier<SubtitlesFile> expectedResult) throws Exception {
         SubtitlesFile actualResult = new Parser<>(format).parse(text);
-        assertThat(actualResult).usingRecursiveComparison().isEqualTo(expectedResult);
+        assertThat(actualResult).usingRecursiveComparison().isEqualTo(expectedResult.get());
     }
 
     public static Stream<Arguments> parse() {
--- src/test/java/io/github/mdaubie/subtitlesparser/TestObjects.java
+++ src/test/java/io/github/mdaubie/subtitlesparser/TestObjects.java
@@ -4,37 +4,40 @@ import io.github.mdaubie.subtitlesparser.model.SubRipFile;
 import io.github.mdaubie.subtitlesparser.model.SubRipSubtitle;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
+//Using supplier to deep copy and keep the objects independent between tests
 public class TestObjects {
     public static class SubRip {
-        private static final SubRipSubtitle sub1 = getSubRipSubtitle(1,
+        public static final Supplier<SubRipSubtitle> sub1 = () -> getSubRipSubtitle(1,
                 LocalTime.of(0, 0, 51, 93 * 1000000),
                 LocalTime.of(0, 0, 52, 635 * 1000000),
-                "(ALARM RINGING)");
-        private static final SubRipSubtitle sub2 = getSubRipSubtitle(2,
+                "This is the first dialogue");
+        public static final Supplier<SubRipSubtitle> sub2 = () -> getSubRipSubtitle(2,
                 LocalTime.of(0, 0, 56, 473 * 1000000),
                 LocalTime.of(0, 0, 58, 266 * 1000000),
-                "(FOOTSTEPS THUDDING)");
-        public static final SubRipFile object1 = getSubRipFile(sub1, sub2);
+                "This is the second dialogue");
+        public static final Supplier<SubRipFile> object1 = () -> getSubRipFile(sub1.get(), sub2.get());
         public static final String text1 = """
                 1
                 00:00:51,093 --> 00:00:52,635
-                (ALARM RINGING)
+                This is the first dialogue
 
                 2
                 00:00:56,473 --> 00:00:58,266
-                (FOOTSTEPS THUDDING)
+                This is the second dialogue
 
                 """;
 
-        static SubRipFile getSubRipFile(SubRipSubtitle... subtitles) {
+        private static SubRipFile getSubRipFile(SubRipSubtitle... subtitles) {
             SubRipFile file = new SubRipFile();
-            file.subtitles = List.of(subtitles);
+            file.subtitles = new ArrayList<>(List.of(subtitles));
             return file;
         }
 
-        static SubRipSubtitle getSubRipSubtitle(int number, LocalTime start, LocalTime end, String content) {
+        private static SubRipSubtitle getSubRipSubtitle(int number, LocalTime start, LocalTime end, String content) {
             SubRipSubtitle subRipSubtitle = new SubRipSubtitle();
             subRipSubtitle.number = number;
             subRipSubtitle.start = start;
