43fc8966268dc4ea23ce9157c1baa9ce6f27fce0
DaubieMatthieu <matthieu.daubie@eleve.isep.fr>
2023-05-07 17:56:29 +0200

    docs: document the model

--- src/main/java/io/github/mdaubie/subtitlesparser/model/Format.java
+++ src/main/java/io/github/mdaubie/subtitlesparser/model/Format.java
@@ -4,6 +4,11 @@ import io.github.mdaubie.subtitlesparser.constants.TIMESTAMP_FORMATS;
 
 import java.time.format.DateTimeFormatter;
 
+/**
+ * An object describing a subtitles format and providing the rules for it to be parsed and serialized within the library
+ * @param <SF> The base class of the format
+ * @see io.github.mdaubie.subtitlesparser.constants.SUB_FILE_FORMATS
+ */
 public record Format<SF extends SubtitlesFile>(String name, String extension,
                                                Class<SF> baseClass,
                                                DateTimeFormatter timestampsFormat) {
